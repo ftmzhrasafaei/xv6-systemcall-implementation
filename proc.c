a03fbeab4e341e1d8a6f95c5fc1fa7fad3c146fc
zara <ftmzhrasafaei@gmail.com>
2021-10-27 01:32:46 -0400

    The 1ed Vesion

--- proc.c
+++ proc.c
@@ -532,3 +532,17 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+int getProcCount(void){
+
+  struct proc *p;
+  int counter = 0;
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    if(p->state != UNUSED){
+      counter++;
+    }
+  }
+  cprintf("Succefully deployed!\n");
+  cprintf("%d \n", counter);
+  return 0;
+}
